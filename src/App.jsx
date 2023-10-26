89ae0feb90239fe7bb64ec9a257243a883b278c7
NaraeJang <j.naaaarae@gmail.com>
2023-10-26 15:58:54 -0400

    create & setup components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Landing,
+  Newsletter,
+  SinglePageError,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <SinglePageError />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
