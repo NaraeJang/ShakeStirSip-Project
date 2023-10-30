2488a5d9dba58699aba4990ded37cd6d3c840f40
NaraeJang <j.naaaarae@gmail.com>
2023-10-30 17:14:33 -0400

    responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,51 @@
+import { NavLink } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { useRef, useState } from "react";
+
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  const toggleLinks = () => {
+    console.log(linksContainerRef.current.getBoundingClientRect());
+    console.log(linksRef.current.getBoundingClientRect());
+    setShowLinks(!showLinks);
+    console.log(showLinks);
+  };
+
+  const linksStyle = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : "0px",
+  };
+
+  console.log(showLinks);
   return (
-    <nav>
+    <nav className="nav">
       <div className="nav-center">
-        <span className="logo">ShakeStirSip</span>
+        <div className="nav-header">
+          <span className="logo">ShakeStirSip</span>
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linksStyle}>
+          <div className="nav-links" ref={linksRef}>
+            <NavLink to="/" className="nav-link">
+              home
+            </NavLink>
+            <NavLink to="/about" className="nav-link">
+              about
+            </NavLink>
+            <NavLink to="newsletter" className="nav-link">
+              newsletter
+            </NavLink>
+          </div>
+        </div>
       </div>
     </nav>
   );
